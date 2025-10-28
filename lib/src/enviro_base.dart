1f16416c4a037425bf1731138529e0ea774e5569
Ijas Hussain <ijashussain3@gmail.com>
2025-10-29 04:47:26 +0530

    Minor improvements

--- lib/src/enviro_base.dart
+++ lib/src/enviro_base.dart
@@ -1,8 +1,3 @@
 // This file serves as a trigger for the enviro generator
 // The actual Enviro class and EnviroEnvironment enum will be generated
-// in enviro.g.dart after running build_runner
-
-/// Placeholder - this will be replaced by generated code
-class _EnviroPlaceholder {
-  const _EnviroPlaceholder._();
-}
+// in lib/gen/enviro.gen.dart after running build_runner
